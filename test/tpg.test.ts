a6ad0c798982911152a77ddcdc635645e6bb9139
oyako-li <oyakoli3@gmail.com>
2024-02-24 16:21:08 +0900

    update test

---
+++ test/tpg.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "node:test";
+import {expect, jest, test} from '@jest/globals';
+import { Tpg } from "../src/tpg";
+
+describe("testing",()=>{
+    it("test 1", ()=>{
+        expect(1).toEqual(1);
+    })
+})
